a83c038e2a85e24953e24a966f058d73635824ce
geneaky <kaktus418@gmail.com>
2022-01-16 19:54:21 +0900

    split based coma, split based parentheses

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,24 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+    
+    @Test
+    public void splitBaseComa() throws Exception {
+        //given
+        //when
+        String[] actual = "1,2".split(",");
+        //then
+        assertThat(actual).contains("1","2");
+        assertThat(actual).containsExactly("1", "2");
+    }
+    
+    @Test
+    public void splitBaseComaWithoutParentheses() throws Exception {
+        //given
+        String givenValue = "(1,2)";
+        //when
+        String actual = givenValue.substring(1,givenValue.length()-1);
+        //then
+        assertThat(actual).isEqualTo("1,2");
+    }
 }
