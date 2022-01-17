2141baf06b92ae45b6925367cb4c4c9a4a1b0ddd
geneaky <kaktus418@gmail.com>
2022-01-17 21:50:50 +0900

    [test] java set api contains @csvsource

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -8,6 +8,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
@@ -32,5 +33,11 @@ public class SetTest {
   @ValueSource(ints = {1,2,3})
   public void set_내용_정확히_일치_테스트(int value) throws Exception {
     assertThat(numbers.contains(value)).isTrue();
+  }
+
+  @ParameterizedTest
+  @CsvSource(value = {"1:true","2:true","3:true","4:false","5:false"}, delimiter = ':')
+  public void set_내용과_일치하지_않는_인자_입력_테스트(int value, boolean result) throws Exception {
+    assertThat(numbers.contains(value)).isEqualTo(result);
   }
 }
