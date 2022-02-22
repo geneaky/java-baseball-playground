ef66ff028d9e27e586d20d159bd239e9cd32fa61
주창륜 <jcr@juchanglyun-ui-MacBookAir.local>
2022-02-22 15:02:16 +0900

    refactor

--- src/main/java/baseball/RandomNumberGenerator.java
+++ src/main/java/baseball/RandomNumberGenerator.java
@@ -8,18 +8,16 @@ import java.util.Random;
 public class RandomNumberGenerator {
 
     public static String generate() {
-        List<Integer> numbers = new ArrayList<>();
-        for(int i = 1; i <= 9; i++) {
-            numbers.add(i);
-        }
-        StringBuilder sb = new StringBuilder();
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9));
         Random random = new Random();
+        StringBuilder sb = new StringBuilder();
 
         for(int i = 0 ; i < 3; i++) {
             Integer number = numbers.get(random.nextInt(numbers.size() - 1));
             sb.append(number);
             numbers.remove(number);
         }
+
         return sb.toString();
     }
 
